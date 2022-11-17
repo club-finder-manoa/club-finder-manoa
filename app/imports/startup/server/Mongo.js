cc2719c1402b5f81723fd3ea372a2b6a8874f853
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-11-17 01:17:06 -1000

    Update mongo startup

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,13 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
-import { Projects } from '../../api/projects/Projects';
-import { ProjectsInterests } from '../../api/projects/ProjectsInterests';
 import { Clubs } from '../../api/clubs/Clubs';
-import { ProfilesProjects } from '../../api/clubs/ProfilesProjects';
-import { ProfilesInterests } from '../../api/clubs/ProfilesInterests';
 import { Users } from '../../api/users/Users';
-import { Stuffs } from '../../api/stuff/Stuff';
 
 /* eslint-disable no-console */
 
@@ -20,71 +15,36 @@ function createUser(email, role) {
   }
 }
 
-/** Define an interest.  Has no effect if interest already exists. */
-function addInterest(interest) {
-  Users.collection.update({ name: interest }, { $set: { name: interest } }, { upsert: true });
-}
-
 /** Defines a new user and associated profile. Error if user already exists. */
-function addProfile({ picture, clubName, clubType, description, contactName, contactEmail, interests, projects, email, role }) {
-  console.log(`Defining profile ${email}`);
-  // Define the user in the Meteor accounts package.
-  createUser(email, role);
+function addClub({ clubName, clubType, mainPhoto, description, tags, relevantMajors, meetingInfo, contactName, contactEmail, photos, admins }) {
+  console.log(`Defining club: ${clubName}`);
   // Create the profile.
-  Clubs.collection.insert({ picture, clubName, clubType, description, contactName, contactEmail, email });
-  // Add interests and projects.
-  interests.map(interest => ProfilesInterests.collection.insert({ profile: email, interest }));
-  projects.map(project => ProfilesProjects.collection.insert({ profile: email, project }));
-  // Make sure interests are defined in the Users collection if they weren't already.
-  interests.map(interest => addInterest(interest));
+  Clubs.collection.insert({ clubName, clubType, mainPhoto, description, tags, relevantMajors, meetingInfo, contactName, contactEmail, photos, admins });
 }
 
-/** Define a new project. Error if project already exists.  */
-function addProject({ name, homepage, description, interests, picture }) {
-  console.log(`Defining project ${name}`);
-  Projects.collection.insert({ name, homepage, description, picture });
-  interests.map(interest => ProjectsInterests.collection.insert({ project: name, interest }));
-  // Make sure interests are defined in the Users collection if they weren't already.
-  interests.map(interest => addInterest(interest));
+function addUserToCollection({ email, savedClubs, interests, major, adminForClubs }) {
+  console.log(`Defining user data: ${email}`);
+  // Create the profile.
+  Users.collection.insert({ email, savedClubs, interests, major, adminForClubs });
 }
-
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
 
 /** Initialize DB if it appears to be empty (i.e. no users defined.) */
 if (Meteor.users.find().count() === 0) {
-  if (Meteor.settings.defaultProjects && Meteor.settings.defaultProfiles) {
-    console.log('Creating the default profiles');
-    Meteor.settings.defaultProfiles.map(profile => addProfile(profile));
-    console.log('Creating the default projects');
-    Meteor.settings.defaultProjects.map(project => addProject(project));
+  if (Meteor.settings.defaultUserData) {
+    console.log('Loading user data');
+    Meteor.settings.defaultAccounts.map(profile => createUser(profile));
+    Meteor.settings.defaultUserData.map(profile => addUserToCollection(profile));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
 
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
+/** Init clubs in DB */
+if (Clubs.collection.find().count() === 0) {
+  if (Meteor.settings.clubs) {
+    console.log('Loading clubz');
+    Meteor.settings.clubs.map(club => addClub(club));
+  } else {
+    console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
-}
-
-/**
- * If the loadAssetsFile field in settings.development.json is true, then load the data in private/data.json.
- * This approach allows you to initialize your system with large amounts of data.
- * Note that settings.development.json is limited to 64,000 characters.
- * We use the "Assets" capability in Meteor.
- * For more info on assets, see https://docs.meteor.com/api/assets.html
- * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
- */
-if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 7)) {
-  const assetsFileName = 'data.json';
-  console.log(`Loading data from private/${assetsFileName}`);
-  const jsonData = JSON.parse(Assets.getText(assetsFileName));
-  jsonData.profiles.map(profile => addProfile(profile));
-  jsonData.projects.map(project => addProject(project));
 }
