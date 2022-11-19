e5750a1a764369217c5ac14a7fbca45807a95421
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-11-19 12:12:37 -1000

    Complete club list view

--- app/imports/ui/pages/AllClubs.jsx
+++ app/imports/ui/pages/AllClubs.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Badge, Container, Card, Image, Row, Col, Button } from 'react-bootstrap';
+import { Badge, Container, Card, Image, Row, Col, Button, Table } from 'react-bootstrap';
 import { List, Grid } from 'react-bootstrap-icons';
 import { useTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Clubs } from '../../api/clubs/Clubs';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { pageStyle } from './pageStyles';
 import { PageIDs } from '../utilities/ids';
 
-/* Component for layout out a club Card. */
+/* Component for club card. */
 const MakeCard = ({ club }) => (
   <Col>
     <a style={{ color: 'black', textDecoration: 'none' }} href="/TempClubPage">
@@ -25,6 +26,39 @@ const MakeCard = ({ club }) => (
   </Col>
 );
 
+MakeCard.propTypes = {
+  club: PropTypes.shape({
+    mainPhoto: PropTypes.string,
+    clubName: PropTypes.string,
+    clubType: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
+
+/* Component for club table item */
+const ClubTableItem = ({ club }) => (
+  <tr>
+    <td style={{ fontWeight: 600 }}>
+      <Link to="/TempClubPage" style={{ textDecoration: 'none', color: 'black' }}>
+        {club.mainPhoto ? <Image src={club.mainPhoto} width={50} /> : ''}&nbsp;&nbsp;&nbsp;{club.clubName}
+      </Link>
+    </td>
+    <td>{club.clubType}</td>
+    <td>
+      {club.tags ? club.tags.map((tag, index) => <Badge key={index} className="my-auto" bg="info">{tag}</Badge>) : ''}
+    </td>
+  </tr>
+);
+
+ClubTableItem.propTypes = {
+  club: PropTypes.shape({
+    mainPhoto: PropTypes.string,
+    clubName: PropTypes.string,
+    clubType: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
+
 const viewButtonStyleSelected = {
   backgroundColor: '#388a60',
   borderWidth: 0,
@@ -35,16 +69,8 @@ const viewButtonStyleSelected = {
 const viewButtonStyle = {
   backgroundColor: 'white',
   borderWidth: 0,
+  borderRadius: '25px',
   color: 'grey',
-};
-
-MakeCard.propTypes = {
-  club: PropTypes.shape({
-    mainPhoto: PropTypes.string,
-    clubName: PropTypes.string,
-    clubType: PropTypes.string,
-    tags: PropTypes.arrayOf(PropTypes.string),
-  }).isRequired,
 };
 
 /* Renders the Profile Collection as a set of Cards. */
@@ -69,7 +95,20 @@ const AllClubs = () => {
         </Row>
       );
     }
-    return 'hi there';
+    return (
+      <Table striped bordered>
+        <thead style={{ position: 'sticky', top: 0, backgroundColor: 'white' }}>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clubs.map((club) => <ClubTableItem key={club._id} club={club} />)}
+        </tbody>
+      </Table>
+    );
   }
 
   return (
@@ -82,11 +121,11 @@ const AllClubs = () => {
           </h1>
         </Col>
         <Col className="text-end my-auto">
-          <Button style={cardView ? viewButtonStyle : viewButtonStyleSelected} onClick={() => setCardView(false)}>
-            <List /> List View
-          </Button>
           <Button style={cardView ? viewButtonStyleSelected : viewButtonStyle} onClick={() => setCardView(true)}>
             <Grid /> Card View
+          </Button>
+          <Button style={cardView ? viewButtonStyle : viewButtonStyleSelected} onClick={() => setCardView(false)}>
+            <List /> List View
           </Button>
         </Col>
       </Row>
