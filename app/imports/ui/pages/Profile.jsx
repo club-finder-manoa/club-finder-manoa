136baeffe3124d5479bf0957fa8c5df098c8dcb1
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-11-30 13:00:31 -1000

    Add 'change password' button/modal

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Card, Col, Container, Image, Modal, Row } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { Users } from '../../api/users/Users';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-/* A simple static component to render some text for the landing page. */
+// Popup modal to confirm reset password
+const ChangePwModal = ({ userId }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const changePw = (newPw) => {
+    Meteor.call('changePw', { userId, newPw });
+    handleClose();
+    // eslint-disable-next-line no-alert
+    alert('Password successfully updated.');
+  };
+
+  const changeStyle = {
+    borderWidth: 0,
+    padding: 0,
+    backgroundColor: 'transparent',
+    color: '#0878A9',
+  };
+
+  return (
+    <>
+      <Button style={changeStyle} onClick={handleShow}>
+        <u>Change Password</u>
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Container className="mt-2">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h3><b>Change Password</b></h3>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="pb-4">
+            Old password:
+            New password:
+            Confirm new password:
+          </Modal.Body>
+          <Modal.Footer className="text-center">
+            <Button variant="light" onClick={handleClose}>
+              Back
+            </Button>
+            <Button variant="info" onClick={() => changePw()}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Container>
+      </Modal>
+    </>
+  );
+};
+
+ChangePwModal.propTypes = {
+  userId: PropTypes.string.isRequired,
+};
+
 const Profile = () => {
   const { ready, userProfile } = useTracker(() => {
     // Ensure that minimongo is populated with all collections prior to running render().
@@ -56,6 +112,11 @@ const Profile = () => {
       <Row>
         <Col className="d-flex justify-content-center py-3">
           <Link to={`/edit-profile/${userProfile._id}`} className="btn btn-primary">Edit Profile</Link>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center pb-3">
+          <ChangePwModal userId={userProfile.accountID} />
         </Col>
       </Row>
     </Container>
