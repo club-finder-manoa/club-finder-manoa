791fb50cd3fe1777f5097efe2aa3f28739746a00
Galen Chang <galenc12@gmail.com>
2022-12-01 11:05:16 -1000

    Update EditProfile.jsx

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -5,6 +5,7 @@ import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { useParams } from 'react-router';
+import swal from 'sweetalert';
 import { Users } from '../../api/users/Users';
 import { Clubs } from '../../api/clubs/Clubs';
 import LoadingSpinner from '../components/LoadingSpinner';
@@ -55,7 +56,11 @@ const EditProfile = () => {
   /* On submit, insert the data. */
   const submit = (data) => {
     const { email, firstName, lastName, aboutMe, picture, interests } = data;
-    Meteor.call('updateUser', { email, firstName, lastName, aboutMe, picture, interests });
+    if (Meteor.call('updateUser', { email, firstName, lastName, aboutMe, picture, interests })) {
+      swal('Error', 'Something went wrong.', 'error');
+    } else {
+      swal('Success', 'Profile updated successfully', 'success');
+    }
   };
   // Now create the model with all the user information.
   return ready ? (
