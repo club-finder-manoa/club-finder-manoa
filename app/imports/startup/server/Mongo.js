a342fab460a2b2a37e655890c588cc9270d3964e
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-12-02 22:47:14 -1000

    Add removeClub function

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -17,6 +17,17 @@ Meteor.methods({
     return Users.collection.update({ email: email }, { $set: { savedClubs: clubArray } });
   },
 
+  removeClub: function ({ email, clubName }) {
+    const clubArray = Users.collection.find({ email: email }).fetch()[0].savedClubs;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const i in clubArray) {
+      if (clubArray[i] === clubName) {
+        clubArray.splice(i, 1);
+      }
+    }
+    return Users.collection.update({ email: email }, { $set: { savedClubs: clubArray } });
+  },
+
   updateUser: function ({ email, firstName, lastName, aboutMe, picture, interests }) {
     return Users.collection.update({ email: email }, { $set: { firstName, lastName, aboutMe, picture, interests } });
   },
