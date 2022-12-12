c5280ac0d5200ee74835f42dcbdcd8231d91ef24
Galen Chang <galenc12@gmail.com>
2022-12-11 20:30:02 -1000

    Merge branch 'issue-52'

--- app/imports/ui/pages/ClubPage.jsx
+++ app/imports/ui/pages/ClubPage.jsx
@@ -181,146 +181,147 @@ const ClubPage = () => {
   }
 
   return ready ? (
-    <Container className="my-3" id="club-info-page">
-      {updateTitleAndCheck()}
-      {club ? (
-        <div>
-          <Row className="align-middle text-center mb-3">
-            <Col>
-              <Row className="my-3">
-                <Col>
-                  <Image src={club.mainPhoto} width={200} />
+    <div className="backgroundImageTop">
+      <Container className="my-3" id="club-info-page">
+        {updateTitleAndCheck()}
+        {club ? (
+          <div>
+            <Row className="align-middle text-center mb-3">
+              <Col>
+                <Row className="my-3">
+                  <Col>
+                    <Image src={club.mainPhoto} width={200} />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    {clubSaved ?
+                      <div>Remove from <i>My Clubs:</i>&nbsp;&nbsp;<RemoveClubModal buttonText="Remove" clubName={club.clubName} email={Meteor.user()?.username} /></div> :
+                      <div>Save to <i>My Clubs:</i>&nbsp;&nbsp;<SaveClubModal clubName={club.clubName} email={Meteor.user()?.username} /></div>}
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col xs={8} className="d-flex flex-column justify-content-center">
+                <h2><b>{club.clubName}</b></h2>
+                <h3 className="mb-3">{club.clubType} Club</h3>
+                <h5 className="mb-3">Visit us at {club.website}</h5>
+                <h5 className="text-start">About us:</h5>
+                <p className="text-start">{club.description}</p>
+              </Col>
+            </Row>
+            {(Roles.userIsInRole(Meteor.userId(), 'admin') || user.adminForClubs?.includes(club.clubName)) ? (
+              <Row className="mt-2 mb-4">
+                <p>Tags</p>
+                <Col className="d-flex">
+                  {club.tags ? club.tags.map((tag, index) => (
+                    <Badge
+                      key={index}
+                      className="rounded-pill"
+                      style={{ fontSize: '20px', fontWeight: 600, paddingTop: '0px', paddingBottom: '0px', paddingStart: '15px', paddingEnd: '15px' }}
+                      bg="secondary"
+                    >&nbsp;{tag} <RemoveTagModal tagToRemove={tag} club={club} />
+                    </Badge>
+                  )) : ''}
+                  <AddTagModal club={club} />
                 </Col>
               </Row>
+            ) : (
+              <Row className="mt-2 mb-4">
+                <h5>Club Tags</h5>
+                <Col className="d-flex">
+                  {club.tags ? club.tags.map((tag, index) => (
+                    <Badge
+                      key={index}
+                      className="rounded-pill"
+                      style={{ fontSize: '20px', fontWeight: 600, paddingTop: '1px', paddingBottom: '3px', paddingStart: '15px', paddingEnd: '15px' }}
+                      bg="secondary"
+                    >&nbsp;{tag}
+                    </Badge>
+                  )) : ''}
+                </Col>
+              </Row>
+            )}
+            <h5><b>Meeting Times and Location</b></h5>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Location</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Sunday</td>
+                  <td>{club.meetingTimeSunday}</td>
+                  <td>{club.meetingLocationSunday}</td>
+                </tr>
+                <tr>
+                  <td>Monday</td>
+                  <td>{club.meetingTimeMonday}</td>
+                  <td>{club.meetingLocationMonday}</td>
+                </tr>
+                <tr>
+                  <td>Tuesday</td>
+                  <td>{club.meetingTimeTuesday}</td>
+                  <td>{club.meetingLocationTuesday}</td>
+                </tr>
+                <tr>
+                  <td>Wednesday</td>
+                  <td>{club.meetingTimeWednesday}</td>
+                  <td>{club.meetingLocationWednesday}</td>
+                </tr>
+                <tr>
+                  <td>Thursday</td>
+                  <td>{club.meetingTimeThursday}</td>
+                  <td>{club.meetingLocationThursday}</td>
+                </tr>
+                <tr>
+                  <td>Friday</td>
+                  <td>{club.meetingTimeFriday}</td>
+                  <td>{club.meetingLocationFriday}</td>
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>{club.meetingTimeSaturday}</td>
+                  <td>{club.meetingLocationSaturday}</td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <h5><b>Contact</b></h5>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{club.contactName}</td>
+                  <td>{club.contactEmail}</td>
+                </tr>
+              </tbody>
+            </Table>
+            {(Roles.userIsInRole(Meteor.userId(), 'admin') || user.adminForClubs?.includes(club.clubName)) ? (
               <Row>
-                <Col>
-                  {clubSaved ?
-                    <div>Remove from <i>My Clubs:</i>&nbsp;&nbsp;<RemoveClubModal buttonText="Remove" clubName={club.clubName} email={Meteor.user()?.username} /></div> :
-                    <div>Save to <i>My Clubs:</i>&nbsp;&nbsp;<SaveClubModal clubName={club.clubName} email={Meteor.user()?.username} /></div>}
+                <Col className="d-flex justify-content-center py-3">
+                  <Link to={`/edit-club/${club._id}`} className="btn btn-primary" id="edit-profile-btn">Edit Club</Link>
                 </Col>
               </Row>
-            </Col>
-
-            <Col xs={8} className="d-flex flex-column justify-content-center">
-              <h2><b>{club.clubName}</b></h2>
-              <h3 className="mb-3">{club.clubType} Club</h3>
-              <h5 className="mb-3">Visit us at {club.website}</h5>
-              <h5 className="text-start">About us:</h5>
-              <p className="text-start">{club.description}</p>
-            </Col>
-          </Row>
-          {(Roles.userIsInRole(Meteor.userId(), 'admin') || user.adminForClubs?.includes(club.clubName)) ? (
-            <Row className="mt-2 mb-4">
-              <p>Tags</p>
-              <Col className="d-flex">
-                {club.tags ? club.tags.map((tag, index) => (
-                  <Badge
-                    key={index}
-                    className="rounded-pill"
-                    style={{ fontSize: '20px', fontWeight: 600, paddingTop: '0px', paddingBottom: '0px', paddingStart: '15px', paddingEnd: '15px' }}
-                    bg="secondary"
-                  >&nbsp;{tag} <RemoveTagModal tagToRemove={tag} club={club} />
-                  </Badge>
-                )) : ''}
-                <AddTagModal club={club} />
-              </Col>
-            </Row>
-          ) : (
-            <Row className="mt-2 mb-4">
-              <h5>Club Tags</h5>
-              <Col className="d-flex">
-                {club.tags ? club.tags.map((tag, index) => (
-                  <Badge
-                    key={index}
-                    className="rounded-pill"
-                    style={{ fontSize: '20px', fontWeight: 600, paddingTop: '1px', paddingBottom: '3px', paddingStart: '15px', paddingEnd: '15px' }}
-                    bg="secondary"
-                  >&nbsp;{tag}
-                  </Badge>
-                )) : ''}
-              </Col>
-            </Row>
-          )}
-          <h5><b>Meeting Times and Location</b></h5>
-          <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Location</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Sunday</td>
-                <td>{club.meetingTimeSunday}</td>
-                <td>{club.meetingLocationSunday}</td>
-              </tr>
-              <tr>
-                <td>Monday</td>
-                <td>{club.meetingTimeMonday}</td>
-                <td>{club.meetingLocationMonday}</td>
-              </tr>
-              <tr>
-                <td>Tuesday</td>
-                <td>{club.meetingTimeTuesday}</td>
-                <td>{club.meetingLocationTuesday}</td>
-              </tr>
-              <tr>
-                <td>Wednesday</td>
-                <td>{club.meetingTimeWednesday}</td>
-                <td>{club.meetingLocationWednesday}</td>
-              </tr>
-              <tr>
-                <td>Thursday</td>
-                <td>{club.meetingTimeThursday}</td>
-                <td>{club.meetingLocationThursday}</td>
-              </tr>
-              <tr>
-                <td>Friday</td>
-                <td>{club.meetingTimeFriday}</td>
-                <td>{club.meetingLocationFriday}</td>
-              </tr>
-              <tr>
-                <td>Saturday</td>
-                <td>{club.meetingTimeSaturday}</td>
-                <td>{club.meetingLocationSaturday}</td>
-              </tr>
-            </tbody>
-          </Table>
-
-          <h5><b>Contact</b></h5>
-          <Table striped bordered hover size="sm">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{club.contactName}</td>
-                <td>{club.contactEmail}</td>
-              </tr>
-            </tbody>
-          </Table>
-          {(Roles.userIsInRole(Meteor.userId(), 'admin') || user.adminForClubs?.includes(club.clubName)) ? (
-            <Row>
-              <Col className="d-flex justify-content-center py-3">
-                <Link to={`/edit-club/${club._id}`} className="btn btn-primary" id="edit-profile-btn">Edit Club</Link>
-              </Col>
-            </Row>
-          ) : ''}
-        </div>
-      ) : (
-        <Col className="text-center mt-3">
-          <h3><b>Page Not Found</b></h3>
-          <br />
-          <Image className="mt-2" src="https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg" />
-        </Col>
-      )}
-
-    </Container>
+            ) : ''}
+          </div>
+        ) : (
+          <Col className="text-center mt-3">
+            <h3><b>Page Not Found</b></h3>
+            <br />
+            <Image className="mt-2" src="https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg" />
+          </Col>
+        )}
+      </Container>
+    </div>
   ) : <LoadingSpinner />;
 };
 
--- app/imports/ui/pages/EditClub.jsx
+++ app/imports/ui/pages/EditClub.jsx
@@ -91,153 +91,154 @@ const EditClub = () => {
   };
 
   return ready ? (
-    <Container id={PageIDs.editClubPage} className="my-3">
-      {updateTitleAndCheck()}
-      {club ? (
-        <div>
-          <AutoForm model={club} schema={bridge} onSubmit={data => submit(data)}>
-            <Row className="align-middle text-center mb-3">
-              <Col>
-                <Row className="my-3">
-                  <Col>
-                    <Image src={club.mainPhoto} width={200} />
-                    <TextField id={ComponentIDs.mainPhoto} name="mainPhoto" showInlineError placeholder={club.mainPhoto} />
-                  </Col>
-                </Row>
-              </Col>
-              <Col xs={8} className="d-flex flex-column justify-content-center">
-                <TextField id={ComponentIDs.clubName} name="clubName" showInlineError placeholder={club.clubName} />
-                <SelectField
-                  id={ComponentIDs.clubType}
-                  as="select"
-                  name="clubType"
-                >
-                  {clubTypes.map((type, key) => <option value={type} key={key}>{type}</option>)}
-                </SelectField>
-                <TextField className="pt-3" name="website" showInlineError placeholder={club.website} />
-                <LongTextField id={ComponentIDs.clubDescription} name="description" showInlineError placeholder={club.description} />
-              </Col>
-            </Row>
-            <Row className="mt-2 mb-4">
-              <p>Tags</p>
-              <Col className="d-flex">
-                {club.tags ? club.tags.map((tag, index) => (
-                  <Badge
-                    key={index}
-                    className="rounded-pill"
-                    style={{ fontSize: '20px', fontWeight: 600, paddingTop: '1px', paddingBottom: '3px', paddingStart: '15px', paddingEnd: '15px' }}
-                    bg="secondary"
-                  >&nbsp;{tag}
-                  </Badge>
-                )) : ''}
-              </Col>
-            </Row>
-            <h5><b>Meeting Times and Location</b></h5>
-            <Table striped bordered hover size="sm">
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Location</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Sunday</td>
-                  <td>
-                    <TextField name="meetingTimeSunday" showInlineError placeholder={club.meetingTimeSunday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationSunday" showInlineError placeholder={club.meetingLocationSunday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Monday</td>
-                  <td>
-                    <TextField name="meetingTimeMonday" showInlineError placeholder={club.meetingTimeMonday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationMonday" showInlineError placeholder={club.meetingLocationMonday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Tuesday</td>
-                  <td>
-                    <TextField name="meetingTimeTuesday" showInlineError placeholder={club.meetingTimeTuesday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationTuesday" showInlineError placeholder={club.meetingLocationTuesday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Wednesday</td>
-                  <td>
-                    <TextField name="meetingTimeWednesday" showInlineError placeholder={club.meetingTimeWednesday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationWednesday" showInlineError placeholder={club.meetingLocationWednesday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Thursday</td>
-                  <td>
-                    <TextField name="meetingTimeThursday" showInlineError placeholder={club.meetingTimeThursday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationThursday" showInlineError placeholder={club.meetingLocationThursday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Friday</td>
-                  <td>
-                    <TextField name="meetingTimeFriday" showInlineError placeholder={club.meetingTimeFriday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationFriday" showInlineError placeholder={club.meetingLocationFriday} />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Saturday</td>
-                  <td>
-                    <TextField name="meetingTimeSaturday" showInlineError placeholder={club.meetingTimeSaturday} />
-                  </td>
-                  <td>
-                    <TextField name="meetingLocationSaturday" showInlineError placeholder={club.meetingLocationSaturday} />
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
+    <div className="backgroundImageTop">
+      <Container id={PageIDs.editClubPage} className="my-3">
+        {updateTitleAndCheck()}
+        {club ? (
+          <div>
+            <AutoForm model={club} schema={bridge} onSubmit={data => submit(data)}>
+              <Row className="align-middle text-center mb-3">
+                <Col>
+                  <Row className="my-3">
+                    <Col>
+                      <Image src={club.mainPhoto} width={200} />
+                      <TextField id={ComponentIDs.mainPhoto} name="mainPhoto" showInlineError placeholder={club.mainPhoto} />
+                    </Col>
+                  </Row>
+                </Col>
+                <Col xs={8} className="d-flex flex-column justify-content-center">
+                  <TextField id={ComponentIDs.clubName} name="clubName" showInlineError placeholder={club.clubName} />
+                  <SelectField
+                    id={ComponentIDs.clubType}
+                    as="select"
+                    name="clubType"
+                  >
+                    {clubTypes.map((type, key) => <option value={type} key={key}>{type}</option>)}
+                  </SelectField>
+                  <TextField className="pt-3" name="website" showInlineError placeholder={club.website} />
+                  <LongTextField id={ComponentIDs.clubDescription} name="description" showInlineError placeholder={club.description} />
+                </Col>
+              </Row>
+              <Row className="mt-2 mb-4">
+                <p>Tags</p>
+                <Col className="d-flex">
+                  {club.tags ? club.tags.map((tag, index) => (
+                    <Badge
+                      key={index}
+                      className="rounded-pill"
+                      style={{ fontSize: '20px', fontWeight: 600, paddingTop: '1px', paddingBottom: '3px', paddingStart: '15px', paddingEnd: '15px' }}
+                      bg="secondary"
+                    >&nbsp;{tag}
+                    </Badge>
+                  )) : ''}
+                </Col>
+              </Row>
+              <h5><b>Meeting Times and Location</b></h5>
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Location</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Sunday</td>
+                    <td>
+                      <TextField name="meetingTimeSunday" showInlineError placeholder={club.meetingTimeSunday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationSunday" showInlineError placeholder={club.meetingLocationSunday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Monday</td>
+                    <td>
+                      <TextField name="meetingTimeMonday" showInlineError placeholder={club.meetingTimeMonday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationMonday" showInlineError placeholder={club.meetingLocationMonday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Tuesday</td>
+                    <td>
+                      <TextField name="meetingTimeTuesday" showInlineError placeholder={club.meetingTimeTuesday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationTuesday" showInlineError placeholder={club.meetingLocationTuesday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday</td>
+                    <td>
+                      <TextField name="meetingTimeWednesday" showInlineError placeholder={club.meetingTimeWednesday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationWednesday" showInlineError placeholder={club.meetingLocationWednesday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Thursday</td>
+                    <td>
+                      <TextField name="meetingTimeThursday" showInlineError placeholder={club.meetingTimeThursday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationThursday" showInlineError placeholder={club.meetingLocationThursday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Friday</td>
+                    <td>
+                      <TextField name="meetingTimeFriday" showInlineError placeholder={club.meetingTimeFriday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationFriday" showInlineError placeholder={club.meetingLocationFriday} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Saturday</td>
+                    <td>
+                      <TextField name="meetingTimeSaturday" showInlineError placeholder={club.meetingTimeSaturday} />
+                    </td>
+                    <td>
+                      <TextField name="meetingLocationSaturday" showInlineError placeholder={club.meetingLocationSaturday} />
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
 
-            <h5><b>Contact</b></h5>
-            <Table striped bordered hover size="sm">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><TextField id={ComponentIDs.clubContactName} name="contactName" showInlineError placeholder={club.contactName} /></td>
-                  <td><TextField id={ComponentIDs.clubContactEmail} name="contactEmail" showInlineError placeholder={club.contactEmail} /></td>
-                </tr>
-              </tbody>
-            </Table>
-            <Col className="d-flex justify-content-center">
-              <SubmitField id="save-changes-btn" value="Save Changes" />
-            </Col>
-            <ErrorsField />
-          </AutoForm>
-        </div>
-      ) : (
-        <Col className="text-center mt-3">
-          <h3><b>Page Not Found</b></h3>
-          <br />
-          <Image className="mt-2" src="https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg" />
-        </Col>
-      )}
-
-    </Container>
+              <h5><b>Contact</b></h5>
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><TextField id={ComponentIDs.clubContactName} name="contactName" showInlineError placeholder={club.contactName} /></td>
+                    <td><TextField id={ComponentIDs.clubContactEmail} name="contactEmail" showInlineError placeholder={club.contactEmail} /></td>
+                  </tr>
+                </tbody>
+              </Table>
+              <Col className="d-flex justify-content-center">
+                <SubmitField id="save-changes-btn" value="Save Changes" />
+              </Col>
+              <ErrorsField />
+            </AutoForm>
+          </div>
+        ) : (
+          <Col className="text-center mt-3">
+            <h3><b>Page Not Found</b></h3>
+            <br />
+            <Image className="mt-2" src="https://i.kym-cdn.com/photos/images/newsfeed/001/042/619/4ea.jpg" />
+          </Col>
+        )}
+      </Container>
+    </div>
   ) : <LoadingSpinner />;
 };
 
